db11b884ad98daf363c7d0a417fdd2e7a6318b40
sbeinart <sbeinart@iupui.edu>
2013-03-25 19:03:57 -0400

    made changes getting basic working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,173 @@
 ;(function($, window) {
-	 // Some sample Javascript functions:
+	
+	var jQT;
+	
+    // Some sample Javascript functions:
     $(function(){
 	
-	    var jQT = new $.jQTouch({
+	    jQT = new $.jQTouch({
 	        statusBar: 'black-translucent',
 	        preloadImages: []
 	    });
 
     });
+    
+	$('.toggle').click(function(e) {
+		var $t = $(this);
+		var id = $t.attr('href');
+		var target = $(id);
+		
+		if(target.css('display') != 'none') {
+			target.removeClass('show');
+		}
+		else {
+			target.addClass('show');
+		}
+		
+		e.preventDefault();
+	});
+	
+	$('.bars').click(function(e) {
+		var $t = $(this);
+		
+		if($t.hasClass('rotate')) {
+			$t.removeClass('rotate');
+		}
+		else {
+			$t.addClass('rotate');
+		}
+				
+		e.preventDefault();
+	});
+	
+	$('#home').bind('pageAnimationEnd', function(event, info) {
+		if (info.direction == 'in') {
+			$("#map").show();
+			
+			google.maps.event.trigger(map.map, 'resize');
+			
+			map.map.setZoom(map.mapOptions.zoom);
+			map.map.fitBounds(map.bounds);
+					
+		}
+		return false;
+	});
+	
+	$('#new-location').submit(function(e) {
+		
+		var $t      = $(this);
+		var $name   = $t.find('#name');
+		var $street = $t.find('#street');
+		var $city   = $t.find('#city');
+		var $state  = $t.find('#state');
+		var $zip    = $t.find('#zip');
+		
+		var address = [
+			$street.val(),
+			$city.val(),
+			$state.val(),
+			$zip.val()
+		];
+		
+		var obj = {
+			name: $name.val(),
+			address: address.join(' '),
+			street: $street.val(),
+			city: $city.val(),
+			state: $state.val(),
+			zipcode: $zip.val()
+		}
+		
+		map.addMarker(obj, function() {
+			map.home();
+			$name.val('');
+			$street.val('');
+			$city.val('');
+			$state.val('');
+			$zip.val('');
+		});
+		
+		e.preventDefault();
+		
+		return false;
+	});
+	
+	$('#edit-location').submit(function(e) {
+		
+		var $t      = $(this);
+		var $name   = $t.find('#name');
+		var $street = $t.find('#street');
+		var $city   = $t.find('#city');
+		var $state  = $t.find('#state');
+		var $zip    = $t.find('#zip');
+		
+		var address = [
+			$street.val(),
+			$city.val(),
+			$state.val(),
+			$zip.val()
+		];
+		
+		var obj = {
+			name: $name.val(),
+			address: address.join(' '),
+			street: $street.val(),
+			city: $city.val(),
+			state: $state.val(),
+			zipcode: $zip.val()
+		}
+		
+		map.editMarker(obj, function() {
+			map.home();
+			$name.val('');
+			$street.val('');
+			$city.val('');
+			$state.val('');
+			$zip.val('');
+		});
+		
+		e.preventDefault();
+		
+		return false;
+	});
+
+	var map = $('#map').MobileMap({
+		mapOptions: {
+			center: new google.maps.LatLng(40, -86)
+		},
+		callback: {
+			newMarker: function(marker, lat, lng, index) {
+				google.maps.event.addListener(marker, 'click', function() {
+				
+					map.editIndex = index;
+					
+					var row     = map.db.query('markers', function(row) {
+						if(row.ID == index+1) {
+							return true;
+						}
+						return false;
+					});
+					
+					row = row[0];
+					
+					var form    = $('#edit-location');
+					var $name   = form.find('#name');
+					var $street = form.find('#street');
+					var $city   = form.find('#city');
+					var $state  = form.find('#state');
+					var $zip    = form.find('#zip');
+					
+					$name.val(row.name);
+					$street.val(row.street);
+					$city.val(row.city);
+					$state.val(row.state);	
+					$zip.val(row.zipcode);
+					
+					jQT.goTo('#edit', 'slideup');		
+					
+				});
+			}
+		}
+	});
+	
 }(jQuery, this));
