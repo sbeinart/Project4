0400c9649969322bd72e34363a096ad4fecc7ef0
sbeinart <sbeinart@iupui.edu>
2013-03-25 18:29:19 -0400

    New Start

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+;(function($, window) {
+	 // Some sample Javascript functions:
+    $(function(){
+	
+	    var jQT = new $.jQTouch({
+	        statusBar: 'black-translucent',
+	        preloadImages: []
+	    });
+
+    });
+}(jQuery, this));
